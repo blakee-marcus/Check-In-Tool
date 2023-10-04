87245cc2e128de2d3506766e23d2414ab8af5f9a
bm <marcusb733@gmail.com>
2023-10-04 16:59:16 -0700

    feat(register): Add functionality to register form on /register

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
 
+import { ADD_USER } from '../utils/mutations';
 import FormInput from '../components/formInput';
+import Auth from '../utils/auth';
 
 const Register = () => {
+  const [addUser, { error }] = useMutation(ADD_USER);
   const [formState, setFormState] = useState({ Username: '', Password: '', "Confirm Password": '' });
+  const navigate = useNavigate();
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -15,10 +20,25 @@ const Register = () => {
     });
   };
 
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await addUser({
+        variables: { username: formState.Username, password: formState.Password },
+      });
+
+      Auth.login(data.addUser.token);
+      navigate('/checkin');
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <section className='vw-100 vh-100 flex-column flex-center'>
       <h1 className='mb-3'>Create Account</h1>
-      <form className='w-80 mx-auto flex-column justify-center'>
+      <form className='w-80 mx-auto flex-column justify-center' onSubmit={handleFormSubmit}>
         <FormInput handleChange={handleChange} label='Username' value={formState.Username} autoComplete="username"  />
         <FormInput handleChange={handleChange} label='Password' type="password" value={formState.Password} autoComplete="new-password"/>
         <FormInput handleChange={handleChange} label='Confirm Password' value={formState["Confirm Password"]} type="password"/>
@@ -40,6 +60,7 @@ const Register = () => {
           Next
         </button>
       </form>
+      {error && <div>Sign up failed</div>}
     </section>
   );
 };
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,9 @@
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+  mutation AddUser($username: String!, $password: String!) {
+    addUser(username: $username, password: $password) {
+      token
+    }
+  }
+`;
